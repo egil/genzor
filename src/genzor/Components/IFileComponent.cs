914d678ce5838f37d7d51a5b971b485a0d3d27ff
Egil Hansen <egil@assimilated.dk>
2021-03-29 21:39:31 +0000

    Changed FileName to Name in IFIleComponent to match with IFile.Name

--- src/genzor/Components/IFileComponent.cs
+++ src/genzor/Components/IFileComponent.cs
@@ -2,6 +2,9 @@ namespace Genzor.Components
 {
 	public interface IFileComponent
 	{
-		string FileName { get; }
+		/// <summary>
+		/// Gets the name of the file the component renders.
+		/// </summary>
+		string Name { get; }
 	}
 }
