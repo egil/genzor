ac794306b06cd97c962bfc81652492937b027077
Egil Hansen <egil@assimilated.dk>
2021-03-31 23:23:01 +0000

    Added netstandard2.0 as a build targeting to support source generators running in Visual Studio

--- src/genzor/ParameterViewBuilder.cs
+++ src/genzor/ParameterViewBuilder.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.AspNetCore.Components;
